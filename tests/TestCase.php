275d4d8f12e0f489e06c586f0b6c222ec4694044
Brian Faust <brian@faust.de.com>
2024-11-04 13:27:50 +0200

    first commit
    Signed-off-by: Brian Faust <brian@faust.de.com>

---
+++ tests/TestCase.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use BaseCodeOy\PackagePowerPack\TestBench\AbstractTestCase;
+
+/**
+ * @internal
+ */
+abstract class TestCase extends AbstractTestCase
+{
+    //
+}

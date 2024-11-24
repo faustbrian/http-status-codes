f60e571fa1d01bf16e42218277938b37e19c1237
Brian Faust <hello@basecode.sh>
2024-11-24 07:10:14 +0200

    first commit
    Signed-off-by: Brian Faust <hello@basecode.sh>

---
+++ src/HttpStatusText.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+/**
+ * Copyright (C) BaseCode Oy - All Rights Reserved
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace BaseCodeOy\HttpStatusCodes;
+
+enum HttpStatusText: string
+{
+    //
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+/**
+ * Copyright (C) BaseCode Oy - All Rights Reserved
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace Tests;
+
+use BaseCodeOy\PackagePowerPack\TestBench\AbstractTestCase;
+
+/**
+ * @internal
+ */
+abstract class TestCase extends AbstractTestCase
+{
+    //
+}
